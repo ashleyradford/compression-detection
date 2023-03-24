9e103bdb179c843d1324ca4735dc6c9a446562db
Ashley <ashleyradford23@gmail.com>
2023-03-24 01:46:53 -0700

    Clean up server main

--- compdetect_server.c
+++ compdetect_server.c
@@ -20,36 +20,26 @@ void parse_config(struct server_config *configs, char *contents)
     configs->udp_payload_size = atoi(cJSON_GetObjectItem(root, "udp_payload_size")->valuestring);
 }
 
-int main(int argc, char *argv[])
-{
-    // check that port is provided
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s port\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    // --- PRE PROBING PHASE ---
-    unsigned short listen_port = atoi(argv[1]);
-
+struct server_config* pre_probing(unsigned short listen_port) {
     // bind port and accept client connection
     int sock;
     if ((sock = create_socket()) < 0) {
-        return EXIT_FAILURE;
+        return NULL;
     }
 
     if (bind_and_listen(sock, listen_port) < 0) {
-        return EXIT_FAILURE;
+        return NULL;
     }
     
     int new_sock;
     if ((new_sock = accept_connection(sock)) < 0) {
-        return EXIT_FAILURE;
+        return NULL;
     }
 
     // receive message
     char *config_contents;
     if ((config_contents = receive_msg(new_sock)) == NULL) {
-        return EXIT_FAILURE;
+        return NULL;
     }
 
     printf("Config contents received, closing TCP connection.\n");
@@ -59,9 +49,58 @@ int main(int argc, char *argv[])
     // parse received config file
     struct server_config *configs = malloc(sizeof(struct server_config));
     parse_config(configs, config_contents);
+    free(config_contents);
 
-    // --- PROBING PHASE ---
+    return configs;
+}
 
+int post_probing(unsigned short listen_port) {
+    char *msg = "It's me, hi im the problem\n";
+    int sock;
+    if ((sock = create_socket()) < 0) {
+        perror("Error creating port");
+        return -1;
+    }
+
+    if (bind_and_listen(sock, listen_port) < 0) {
+        perror("Error binding port");
+        return -1;
+    }
+
+    int new_sock;
+    if ((new_sock = accept_connection(sock)) < 0) {
+        perror("Error accepting connection");
+        return -1;
+    }
+
+    if (send_msg(new_sock, msg) < 0) {
+        return -1;
+    }
+
+    printf("Congestion status sent, closing TCP connection.\n");
+    close(new_sock);
+    close(sock);
+
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    // check that port is provided
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s port\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    unsigned short listen_port = atoi(argv[1]);
+
+    // pre probing phase
+    struct server_config *configs;
+    if ((configs = pre_probing(listen_port)) == NULL) {
+        return EXIT_FAILURE;
+    };
+
+    // probing phase
     // use a short int
     // incremenet using bit wise op
     // move most sig and lest sig
@@ -70,33 +109,12 @@ int main(int argc, char *argv[])
     // get time of day (better one to use)
     sleep(4);
 
-    // --- POST PROBING PHASE ---
-    char *msg = "It's me, hi im the problem\n";
-    if ((sock = create_socket()) < 0) {
-        perror("Error creating port");
+    // post probing phase
+    if (post_probing(listen_port) < 0) {
         return EXIT_FAILURE;
-    }
+    };
 
-    if (bind_and_listen(sock, listen_port) < 0) {
-        perror("Error binding port");
-        return EXIT_FAILURE;
-    }
-    
-    if ((new_sock = accept_connection(sock)) < 0) {
-        perror("Error accepting connection");
-        return EXIT_FAILURE;
-    }
-
-    if (send_msg(new_sock, msg) < 0) {
-        return EXIT_FAILURE;
-    }
-
-    printf("Congestion status sent, closing TCP connection.\n");
-    close(new_sock);
-    close(sock);
-
-    // free memory
-    free(config_contents);
+    // free config structure data
     free(configs);
 
     return EXIT_SUCCESS;
